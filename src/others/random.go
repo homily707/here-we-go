015fc02c6c6606e4e26b4d91b67fdb55c20255b8
homily707 <homily707@outlook.com>
2022-05-11 20:45:03 +0800

    ()

--- src/others/random.go
+++ src/others/random.go
@@ -8,14 +8,14 @@ func main() {
 	fmt.Println("--------------------")
 
 	peoples := [13]string{
-		"就砺","铭少","莫须","穹谷","锐晟","辛八",
-		"心远","银桑","远跖","中亭","中西","亦余",
+		"就砺", "铭少", "莫须", "穹谷", "锐晟", "辛八",
+		"心远", "银桑", "远跖", "中亭", "中西", "亦余",
 		"自疏"}
 
 	counts := len(peoples)
 
 	var (
-		userInput int
+		userInput  int
 		rollNumber int
 	)
 
@@ -23,34 +23,34 @@ func main() {
 
 		for {
 			fmt.Println("😄 请输入随机数种子：")
-			_, err := fmt.Scanf("%d",&userInput)
-			if(err == nil) {
-				break;
+			_, err := fmt.Scanf("%d", &userInput)
+			if err == nil {
+				break
 			}
 			fmt.Println(err)
 			fmt.Println("🥸 非法的输入，请重试")
 		}
 
 		rand.Seed(int64(userInput))
-		rollNumber = rand.Intn(counts-i)
+		rollNumber = rand.Intn(counts - i)
 
 		//fmt.Println(rollNumber)
 		fmt.Println()
 		fmt.Println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉")
 		fmt.Println("🎉")
-		fmt.Println("🤗 你获得了 "+peoples[rollNumber]+" 的礼物 🎁")
+		fmt.Println("🤗 你获得了 " + peoples[rollNumber] + " 的礼物 🎁")
 		fmt.Println("🎉")
 		fmt.Println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉")
-		fmt.Println("-------------------------") 
+		fmt.Println("-------------------------")
 		fmt.Println()
-		if(i<counts-2) {
-			fmt.Println("😎 请"+peoples[rollNumber]+"开始抽奖 ")
+		if i < counts-2 {
+			fmt.Println("😎 请" + peoples[rollNumber] + "开始抽奖 ")
 		}
 
-		peoples[counts-1-i],peoples[rollNumber] = peoples[rollNumber],peoples[counts-1-i]
+		peoples[counts-1-i], peoples[rollNumber] = peoples[rollNumber], peoples[counts-1-i]
 	}
 
-	fmt.Println("-------------------------") 
+	fmt.Println("-------------------------")
 	fmt.Println()
 	fmt.Println("中亭额外准备了一份礼物🥳")
 	fmt.Println()
@@ -58,24 +58,24 @@ func main() {
 
 	for {
 		fmt.Println("😄 请输入随机数种子：")
-		_, err := fmt.Scanf("%d",&userInput)
-		if(err == nil) {
-			break;
+		_, err := fmt.Scanf("%d", &userInput)
+		if err == nil {
+			break
 		}
 		fmt.Println(err)
 		fmt.Println("🥸 非法的输入，请重试")
 	}
 
 	rand.Seed(int64(userInput))
-	rollNumber = rand.Intn(counts-1)
+	rollNumber = rand.Intn(counts - 1)
 
 	//fmt.Println(rollNumber)
 	fmt.Println()
 	fmt.Println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉")
 	fmt.Println("🎉")
-	fmt.Println("🤗 恭喜 "+peoples[rollNumber]+" 获得了的礼物 🎁")
+	fmt.Println("🤗 恭喜 " + peoples[rollNumber] + " 获得了的礼物 🎁")
 	fmt.Println("🎉")
 	fmt.Println("🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉")
-	fmt.Println("-------------------------") 
+	fmt.Println("-------------------------")
 
 }
