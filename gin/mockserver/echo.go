12cda515e308e260d21a2b114df1813e6de6dbaa
homily707 <homily707@outlook.com>
2022-07-03 01:38:48 +0800

    feat: echo server

--- gin/mockserver/echo.go
+++ gin/mockserver/echo.go
@@ -3,15 +3,23 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func start() {
 	engine := gin.Default()
-	engine.POST("/echo", func(c *gin.Context) {
-		bytes, _ := ioutil.ReadAll(c.Request.Body)
-		c.JSON(http.StatusOK, gin.H{
+	engine.POST("/echo", func(context *gin.Context) {
+		bytes, _ := ioutil.ReadAll(context.Request.Body)
+		log.Println(string(bytes))
+		context.JSON(http.StatusOK, gin.H{
 			"body": string(bytes),
+		})
+	})
+	engine.GET("/echo", func(context *gin.Context) {
+		log.Println(string(context.Request.RequestURI))
+		context.JSON(http.StatusOK, gin.H{
+			"URI": context.Request.RequestURI,
 		})
 	})
 	engine.Run()
